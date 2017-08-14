8c2189626ec74180a97cb15f6ad296ef1fa5db63
1085069832 <1085069832@qq.com>
2017-08-14 14:27:35 +0800

    完成平面和圆柱宏切换

--- Assets/Scripts/PenController.cs
+++ Assets/Scripts/PenController.cs
@@ -4,19 +4,32 @@ using UnityEngine;
 
 public class PenController : MonoBehaviour
 {
+
     [SerializeField] Transform StartPenCursorTransf;
     [SerializeField] Transform startTrsanform;
     [SerializeField] Transform tipTransform;
     RaycastHit raycastHit;
     [SerializeField] GameObject penPoint;
-    PinchDraw pinchDraw;
+#if COLUMN
     [SerializeField] float brushSize = 0.0035f;
+#endif
     [SerializeField] GameObject paper;
     LayerMask layerMask;
+    [SerializeField] Transform cursorHigh;
+    [SerializeField] Transform cursorLow;
+    [SerializeField] Transform cursor;
+    Vector3 oldPos;
+
     // Use this for initialization
     void Start()
     {
-        pinchDraw = GetComponent<PinchDraw>();
+#if PLANE
+        GetComponent<PinchDraw>().enabled = false;
+#endif
+
+#if COLUMN
+        GetComponent<dLineManager>.enabled = false;
+#endif
         layerMask = ~(1 << LayerMask.NameToLayer(MyConst.MIRRORCENTER));
     }
 
@@ -30,7 +43,12 @@ public class PenController : MonoBehaviour
         {
             if (raycastHit.transform.name == MyConst.PAPER)
             {
+#if PLANE
+                penPoint.transform.position = new Vector3(raycastHit.point.x, paper.transform.position.y + 0.001f, raycastHit.point.z);
+#endif
+#if COLUMN
                 penPoint.transform.position = new Vector3(raycastHit.point.x, paper.transform.position.y, raycastHit.point.z);
+#endif
             }
         }
         //笔头
@@ -41,13 +59,32 @@ public class PenController : MonoBehaviour
         {
             if (raycastHit.transform.name == MyConst.PAPER)
             {
-                var penDepth = Mathf.Clamp((startTrsanform.position - raycastHit.point).magnitude, 0.01f, 0.05f);
+                var penDepth = Mathf.Clamp((startTrsanform.position - raycastHit.point).magnitude, 0.01f, 1f);
+#if COLUMN
                 BrushManager.cursorsize = brushSize / (penDepth * 50);
+#endif
+#if PLANE
+                if (oldPos != Vector3.zero)
+                    cursor.LookAt(oldPos);
+                //平面画
+                cursorHigh.localPosition = new Vector3(0.0015f / penDepth, 0, 0);
+                cursorLow.localPosition = new Vector3(-0.0015f / penDepth, 0, 0);
+
+                //记录旧点
+                oldPos = raycastHit.point;
+#endif
             }
         }
         else
         {
+#if COLUMN
             BrushManager.cursorsize = 0;
+#endif
+#if PLANE
+            //平面画
+            cursorHigh.localPosition = Vector3.zero;
+            cursorLow.localPosition = Vector3.zero;
+#endif
         }
     }
 }
